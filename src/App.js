58af44323c19da8af13ffc72107e3014a92928f8
simonwebable <simon@webable.com.bd>
2018-10-26 15:55:52 +0600

    React Sementic UI setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 
 const App = () => (
-  <div>
+  <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
   </div>
